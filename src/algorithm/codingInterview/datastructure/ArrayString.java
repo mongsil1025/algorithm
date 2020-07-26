d77d9fc3c782e8e1506217175f3688eb8be77bd8
sunmi <sunminjsm@gmail.com>
2020-07-26 23:13:16 +0900

    배열과문자열 실습

---
+++ src/algorithm/codingInterview/datastructure/ArrayString.java
@@ -0,0 +1,36 @@
+package algorithm.codingInterview.datastructure;
+
+public class ArrayString {
+
+	/**************
+	 * 
+	 * 문자열이 주어졌을 때, 이 문자열에 같은 문자가 중복되어 등장하는지 확인하는 알고리즘을 작성하라.
+	 * 자료구조를 추가로 사용하지 않고 풀 수 있는 알고리즘 또한 고민하라. 
+	 * 
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		
+		String str = "aksldfwerq";
+		System.out.println(isUniqueChars(str) == true ? "Yes It's unique String" : "No it has duplicated character");
+		
+	}
+	
+	public static boolean isUniqueChars(String str) {
+		if(str.length() > 128) {
+			return false;
+		}
+		boolean[] char_set = new boolean[128];
+		for(int i=0; i<str.length(); i++) {
+			int val = str.charAt(i);
+			if(char_set[val]) {
+				return false; // 중복
+			}
+			char_set[val] = true;
+		}
+		return true;
+	}
+	
+}
